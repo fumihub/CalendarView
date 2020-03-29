2f360db5846791a3245c197a73f88c3a654581c8
fumihub <60814954+fumihub@users.noreply.github.com>
2020-03-29 10:26:24 +0900

    バナー広告の設定
    テスト広告を表示

--- CalenderView/app/src/main/java/com/non_name_hero/calenderview/MainActivity.java
+++ CalenderView/app/src/main/java/com/non_name_hero/calenderview/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.os.Bundle;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
 
     //private TextView titleText;
     private GridView calendarGridView;
     private CalendarAdapter calendarAdapter;
+    private AdView mAdView;
     /*ボタン作成しないためコメントアウト
     private Button prevButton, nextButton;
     private CalendarAdapter mCalendarAdapter;
@@ -18,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //広告の読み込み
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        //UI部分のバナー広告とIDを接続
+        mAdView = findViewById(R.id.adView);
+        //インスタンス生成
+        AdRequest adRequest = new AdRequest.Builder().build();
+        //広告読み込み
+        mAdView.loadAd(adRequest);
 
         //titleText = findViewById(R.id.titleText);
         /*ボタン作成しないためコメントアウト
