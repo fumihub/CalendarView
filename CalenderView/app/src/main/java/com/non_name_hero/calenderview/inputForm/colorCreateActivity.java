ecdd6f6ee1c2bc27c7b0dbdd88e86076fda89a60
nakata <54203719+heyhey1007@users.noreply.github.com>
2020-08-30 19:57:31 +0900

    #11 色関連画面の編集をコミット

--- CalenderView/app/src/main/java/com/non_name_hero/calenderview/inputForm/colorCreateActivity.java
+++ CalenderView/app/src/main/java/com/non_name_hero/calenderview/inputForm/colorCreateActivity.java
@@ -1,11 +1,8 @@
 package com.non_name_hero.calenderview.inputForm;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.non_name_hero.calenderview.R;
+import com.non_name_hero.calenderview.data.ScheduleGroup;
+import com.non_name_hero.calenderview.data.source.ScheduleDataSource;
+import com.non_name_hero.calenderview.data.source.ScheduleRepository;
+import com.non_name_hero.calenderview.utils.Injection;
 
 public class colorCreateActivity extends AppCompatActivity {
 
@@ -25,6 +26,7 @@ public class colorCreateActivity extends AppCompatActivity {
     private Intent intentOut;
 
     private SharedPreferences prefs;
+    private ScheduleRepository repository;
 
     private EditText colorCreateTitle;
 
@@ -43,7 +45,7 @@ public class colorCreateActivity extends AppCompatActivity {
 
     //コンストラクタ
     public colorCreateActivity(){
-
+        repository = Injection.provideScheduleRepository(getApplicationContext());
     }
 
     @Override
@@ -122,7 +124,7 @@ public class colorCreateActivity extends AppCompatActivity {
         }
 
         //エラー処理
-        if (color == 0 || textColor == "" || colorCreateTitle.getText().toString() == "") {
+        if (color == 0 || textColor.isEmpty() || colorCreateTitle.getText().toString().isEmpty()) {
             //トースト表示
             Toast errorToast = Toast.makeText(
                     getApplicationContext(),
@@ -132,25 +134,47 @@ public class colorCreateActivity extends AppCompatActivity {
             errorToast.show();
         }
         else {
-            //色選択遷移用intent
-            intentOut = getIntent();
-            //ボタンの色を遷移先へreturn
-            intentOut.putExtra("Color", color);
-            //色タイトルを遷移先へreturn
-            intentOut.putExtra("ColorTitle", colorCreateTitle.getText().toString());
-            //文字色を遷移先へreturn
-            intentOut.putExtra("textColor", textColor);
-            //ボタンの色番号を遷移先へreturn
-            intentOut.putExtra("ColorNumber", colorNumber);
-            setResult(RESULT_OK, intentOut);
-            //colorNumberをString型に変換
-            String strColorNumber = String.valueOf(colorNumber);
-            //SharedPreferenceに「colorNumber(数字)」の形で保存
-            //↓ここでエラーが起きる
-            /*prefs = getSharedPreferences("SaveData", Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.putBoolean(strColorNumber,Boolean.TRUE);*/
-            finish();
+
+            repository.insertScheduleGroup(
+                    new ScheduleGroup(
+                            colorNumber,
+                            colorCreateTitle.getText().toString(),
+                            textColor,
+                            color
+                    ),
+                    new ScheduleDataSource.SaveScheduleGroupCallback() {
+                        @Override
+                        public void onScheduleGroupSaved() {
+                            finish();
+                        }
+
+                        @Override
+                        public void onDataNotSaved() {
+
+                        }
+                    }
+            );
+
+
+//            //色選択遷移用intent
+//            intentOut = getIntent();
+//            //ボタンの色を遷移先へreturn
+//            intentOut.putExtra("Color", color);
+//            //色タイトルを遷移先へreturn
+//            intentOut.putExtra("ColorTitle", colorCreateTitle.getText().toString());
+//            //文字色を遷移先へreturn
+//            intentOut.putExtra("textColor", textColor);
+//            //ボタンの色番号を遷移先へreturn
+//            intentOut.putExtra("ColorNumber", colorNumber);
+//            setResult(RESULT_OK, intentOut);
+//            //colorNumberをString型に変換
+//            String strColorNumber = String.valueOf(colorNumber);
+//            //SharedPreferenceに「colorNumber(数字)」の形で保存
+//            //↓ここでエラーが起きる
+//            /*prefs = getSharedPreferences("SaveData", Context.MODE_PRIVATE);
+//            SharedPreferences.Editor editor = prefs.edit();
+//            editor.putBoolean(strColorNumber,Boolean.TRUE);*/
+//            finish();
         }
 
     }
