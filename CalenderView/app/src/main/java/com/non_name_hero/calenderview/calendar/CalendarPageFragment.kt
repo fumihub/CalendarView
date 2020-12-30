065d1772fc6ef0021c79841672e281405ff62dab
fumihub <60814954+fumihub@users.noreply.github.com>
2020-12-30 17:48:42 +0900

    #86 スケジュール入力と家計簿入力遷移分け完了
    ・スケジュール入力と家計簿入力遷移分け実装
    ・不要なコードを削除
    【残件】
    ①カレンダー画面での家計簿内容表示
    ②切り替えボタンのアイコン作成
    ③accountingTextの実装

--- CalenderView/app/src/main/java/com/non_name_hero/calenderview/calendar/CalendarPageFragment.kt
+++ CalenderView/app/src/main/java/com/non_name_hero/calenderview/calendar/CalendarPageFragment.kt
@@ -1,22 +1,19 @@
 package com.non_name_hero.calenderview.calendar
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.AdapterView.OnItemLongClickListener
-import android.widget.GridView
-import androidx.databinding.DataBindingUtil
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.non_name_hero.calenderview.R
 import com.non_name_hero.calenderview.databinding.CalendarFragmentScreenSlidePageBinding
 import com.non_name_hero.calenderview.inputForm.InputActivity
-
+import com.non_name_hero.calenderview.inputForm.InputBalanceActivity
 import com.non_name_hero.calenderview.utils.PigLeadUtils
-import java.util.*
-import kotlin.collections.ArrayList
 
 /**
  * フラグメントにカレンダーを表示させるクラス
@@ -63,9 +60,11 @@ class CalendarPageFragment     //コンストラクタ
             binding.viewmodel?.setScheduleItem(dateArray[position])
         }
         binding.calendarGridView.onItemLongClickListener = OnItemLongClickListener { _, _, position, _ -> //入力画面に遷移
-            val intent = Intent(context, InputActivity::class.java)
+            /*SharedPreferenceからbalanceFlagの値を取得*/
+            val prefs: SharedPreferences = this.activity!!.getSharedPreferences("input_balance_data", AppCompatActivity.MODE_PRIVATE)
+            /*スケジュール画面の場合スケジュール入力画面へ、家計簿画面の場合家計簿入力画面へ*/
+            val intent = if (prefs.getBoolean("balanceFlag", false)) Intent(context, InputBalanceActivity::class.java) else Intent(context, InputActivity::class.java)
             val dateArray = (binding.calendarGridView.adapter as CalendarAdapter).dateArray
-//            val dateArray = mCalendarAdapter.dateArray ?: ArrayList<Date>()
             /*TODO スケジュール入力時と家計簿入力時に分ける*/
             //入力画面に引数で年月日を渡す
             val year:Int = Integer.valueOf(PigLeadUtils.yearFormat.format(dateArray[position]))
